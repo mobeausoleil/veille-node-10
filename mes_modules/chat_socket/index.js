437472147b402d5a2b29670643aa856fd42459c2
Maurice Beausoleil <kool_momo_kool@hotmail.com>
2018-03-09 13:20:09 -0500

    chat fini

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -1,3 +1,4 @@
+const util = require("util");
 let socketio = require('socket.io');
 
 module.exports.listen = function(server){
@@ -5,9 +6,32 @@ module.exports.listen = function(server){
 
     // ------------------------------ Traitement du socket
     let objUtilisateur = {}
+    let msgList = {}
     io.on('connection', function(socket){
-    console.log(socket.id)
-    // .......
+	    // .......
+	    socket.on("setUser", function(data){
+
+	    	objUtilisateur[socket.id] = data.user;
+	    	socket.emit("valid_user", data);
+	    	io.sockets.emit("diffuser_list_user", objUtilisateur);
+
+	    });
+
+	    socket.on("setMsg", function(data){
+
+	    	data.user = objUtilisateur[socket.id];
+	    	socket.broadcast.emit("diffuser_message", data);
+	    	socket.emit("valid_message", data);
+
+	    });
+
+	    socket.on("disconnect", function(){
+
+	    	delete objUtilisateur[socket.id];
+	    	io.sockets.emit("diffuser_list_user", objUtilisateur);
+
+	    });
    });
+
  return io;
 }
